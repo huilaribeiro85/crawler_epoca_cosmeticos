1cac291bd4c75fa2d85e8c7f28e97e5f42d6e8fe
huilaribeiro85 <huila.ribeiro@gmail.com>
2019-03-18 21:27:06 -0300

    path_update

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='crawler_epoca_cosmeticos',
+    version='1.0.0',
+    author='Huila Ribeiro',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['requests']
+)
